5f5991721788b5975df620b248de295bd16b0441
georgGrankvist <62744506+georgGrankvist@users.noreply.github.com>
2022-02-12 16:12:32 +0100

    Initial commit

---
+++ src/main/java/com/example/HiQ/BE/HiQBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.HiQ.BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiQBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiQBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HiQ/BE/HiQBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HiQ.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiQBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
